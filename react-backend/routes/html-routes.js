3e851e5ad12c63289403ce5b6913cfec8e2ddc4d
vishnuvalla <vishnuvalla@gmail.com>
2019-05-03 00:20:04 -0500

    update sql

--- react-backend/routes/html-routes.js
+++ react-backend/routes/html-routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, connection){
 
     app.get('/', function(req, res) {
         
-        connection.query('SELECT * FROM test;', function(err, data) {
+        connection.query('SELECT * FROM test_predictions;', function(err, data) {
             (err)?res.send(err):res.json({users: data});
         });
     });
