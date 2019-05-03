c9a0d6a121f36aa84d00ac5ecd876466598abfbc
vishnuvalla <vishnuvalla@gmail.com>
2019-05-03 01:12:24 -0500

    remove get/

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -5,9 +5,7 @@ const mysql = require('mysql')
 
 
 module.exports = function(app,connection){
-  app.get('/', function(req, res) {
     connection.query('SELECT * FROM test_predictions LIMIT 10;', function(err, data) {
       (err)?res.send(err):res.send(JSON.stringify(data));
     });
-  });
 }
