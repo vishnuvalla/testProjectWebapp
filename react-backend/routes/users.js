b9b5d5214d25c5a17d1343b2c6ce132f6a53c3d9
vishnuvalla <vishnuvalla@gmail.com>
2019-05-03 01:06:25 -0500

    users changes

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const mysql = require('mysql')
 
-/* GET users listing. */
-router.get('/', function(app, connection) {
-    connection.query('SELECT * FROM test_predictions LIMIT 10;', function(err, results, fields) {
-      (err)?res.send(err):res.send(JSON.stringify(results));
+
+module.exports = function(app,connection){
+  app.get('/', function(req, res) {
+    connection.query('SELECT * FROM test_predictions LIMIT 10;', function(err, data) {
+      (err)?res.send(err):res.send(JSON.stringify(data));
+    });
   });
-});
-
-module.exports = router;
+}
